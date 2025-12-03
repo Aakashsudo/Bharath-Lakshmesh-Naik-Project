bd180f40f353b7b252f6685578ca7bf36dcfc794
Aakashsudo <79778923+Aakashsudo@users.noreply.github.com>
2025-12-03 15:49:49 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -5,11 +5,11 @@ public class Factorial {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a number:");
-		int n = sc.nextInt();
-        long fact =1;
-        for(int i=1;i<=n;i++) {
-        	fact = fact*i;
+		int number = sc.nextInt();
+        long factorial =1;
+        for(int integer=1;integer<=n;integer++) {
+        	factorial = factorial*i;
         }
-        System.out.println("Factorial of "+n+"="+fact);
+        System.out.println("Factorial of "+n+"="+factorial);
         }
 	}
