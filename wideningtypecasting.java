f7e99f6b8896cf021d42fe24b83c8b0b9c7ae842
Aakashsudo <79778923+Aakashsudo@users.noreply.github.com>
2025-12-03 13:41:35 +0530

    Add files via upload

---
+++ wideningtypecasting.java
@@ -0,0 +1,15 @@
+package year;
+
+public class WideningTypeCastingExample {
+
+	public static void main(String[] args) {
+		int i =16;
+		long l =i;
+		double d =i;
+		System.out.println("int i="+i);
+		System.out.println("long l="+l);
+		System.out.println("double d="+d);
+
+	}
+
+}
