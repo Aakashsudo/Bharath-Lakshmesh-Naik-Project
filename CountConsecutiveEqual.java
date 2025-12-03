7a2deb29710e61c95939325d798d733936222b49
Aakashsudo <79778923+Aakashsudo@users.noreply.github.com>
2025-12-03 15:46:28 +0530

    Rename variable and update loop for clarity

--- CountConsecutiveEqual.java
+++ CountConsecutiveEqual.java
@@ -3,11 +3,11 @@ package year;
 public class CountConsecutiveEqual {
 
 	public static void main(String[] args) {
-		int[] arr = {2, 2, 5, 5, 5, 9};
+		int[] array = {2, 2, 5, 5, 5, 9};
         int count = 0;
 
-        for(int i = 0; i < arr.length - 1; i++) {
-            if(arr[i] == arr[i + 1]) {
+        for(int integer = 0; integer < array.length - 1; integer++) {
+            if(array[integer] == array[integer + 1]) {
                 count++;
             }
         }
