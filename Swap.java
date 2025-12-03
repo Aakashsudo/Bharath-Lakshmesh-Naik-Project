6ad43724466edce4e946865a9157aad60f0175d4
Aakashsudo <79778923+Aakashsudo@users.noreply.github.com>
2025-12-03 14:34:45 +0530

    Create Swap.java

---
+++ Swap.java
@@ -0,0 +1,16 @@
+package year;
+
+public class swap extends arrays2 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		System.out.println("Beforw Swap ; a"+a+",b="+b);
+		int temp = a;
+		      a = b;
+		      b = temp;
+		      System.out.println("After swap : a"+a+",b="+b);
+
+	}
+
+}
