a2fc450eccfdca169f92b9aabab8e1204f064e0e
Aakashsudo <79778923+Aakashsudo@users.noreply.github.com>
2025-12-03 15:11:50 +0530

    Rename variable and update loop for clarity

--- Alphabetlowercase.java
+++ Alphabetlowercase.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Alphabetlowercase {
 
 	public static void main(String[] args) {
-		char ch = 'a';
+		char character = 'a';
 
-        while (ch <= 'z') {
-            System.out.print(ch + " ");
+        while (character <= 'z') {
+            System.out.print(character + " ");
             ch++;
         }
     }
