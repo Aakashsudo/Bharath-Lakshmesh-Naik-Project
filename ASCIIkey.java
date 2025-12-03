25d6b2c412b169f8f7e8c1405b6691893e68927c
Aakashsudo <79778923+Aakashsudo@users.noreply.github.com>
2025-12-03 13:51:38 +0530

    Create ASCIIkey.java

---
+++ ASCIIkey.java
@@ -0,0 +1,13 @@
+package year;
+
+public class ASCII {
+
+	public static void main(String[] args) {
+		char x = '%';
+		
+		System.out.println("R="+x);
+		
+		System.out.println("asci value of x="+(int)x);
+
+}
+}
