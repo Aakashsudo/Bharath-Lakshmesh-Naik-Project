dbf0c57ab02de5f11056eaf1aee6c77a517f871b
Aakashsudo <79778923+Aakashsudo@users.noreply.github.com>
2025-12-03 13:46:17 +0530

    Create Alphabetlowercase.java

---
+++ Alphabetlowercase.java
@@ -0,0 +1,13 @@
+package year;
+import java.util.Scanner;
+public class Alphabetlowercase {
+
+	public static void main(String[] args) {
+		char ch = 'a';
+
+        while (ch <= 'z') {
+            System.out.print(ch + " ");
+            ch++;
+        }
+    }
+}
