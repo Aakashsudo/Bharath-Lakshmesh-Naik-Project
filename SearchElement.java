3fb92ffed3ed8eacd8c18ea3a6ed62ee26979dd3
Aakashsudo <79778923+Aakashsudo@users.noreply.github.com>
2025-12-03 16:12:19 +0530

    Rename variable 'arr' to 'array' in SearchElement

--- SearchElement.java
+++ SearchElement.java
@@ -3,13 +3,13 @@ package year;
 public class SearchElement {
 
 	public static void main(String[] args) {
-	        int[] arr = {4, 7, 2, 9, 12};
+	        int[] array = {4, 7, 2, 9, 12};
 	        int key = 9;
 
 	        boolean found = false;
 
-	        for(int i = 0; i < arr.length; i++) {
-	            if(arr[i] == key) {
+	        for(int integer = 0; integer < array.length; integer++) {
+	            if(array[integer] == key) {
 	                found = true;
 	                break;
 	            }
