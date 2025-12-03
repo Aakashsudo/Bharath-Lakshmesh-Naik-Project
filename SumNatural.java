8d71d7db53d0c7385b7c7aa2b73ae819512185f0
Aakashsudo <79778923+Aakashsudo@users.noreply.github.com>
2025-12-03 14:33:04 +0530

    Create SumNatural.java

---
+++ SumNatural.java
@@ -0,0 +1,21 @@
+package year;
+import java.util.Scanner;
+public class SumNatural {
+
+	public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+        int i = 1;
+
+        while (i <= n) {
+            sum = sum + i;
+            i++;
+        }
+
+        System.out.println("Sum of first " + n + " natural numbers = " + sum);
+    }
+}
