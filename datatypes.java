ab971d8ace7e9dc8725de707845d01377f6c1cd8
Aakashsudo <79778923+Aakashsudo@users.noreply.github.com>
2025-12-03 13:57:13 +0530

    Create datatypes.java

---
+++ datatypes.java
@@ -0,0 +1,19 @@
+package year;
+
+public class Example {
+
+	public static void main(String[] args) {
+		int x = 10000;
+		int y = 20000;
+		long l = x*y;
+		float f = 15/10;
+		double d = x*y;
+		String s ="AKASH Tehcattyyy";
+		System.out.println("x*y :" +l);
+		System.out.println("x*y :" +d);
+		System.out.println("x/y :" +f);
+		System.out.println("s :" +s);
+		
+	}
+
+}
