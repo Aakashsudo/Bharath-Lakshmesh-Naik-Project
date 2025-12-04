a27096dd08a62896b6927524b1d51aaac2a12b5f
Aakashsudo <79778923+Aakashsudo@users.noreply.github.com>
2025-12-04 13:47:15 +0530

    Update Swap.java

--- Swap.java
+++ Swap.java
@@ -3,13 +3,13 @@ package year;
 public class swap extends arrays2 {
 
 	public static void main(String[] args) {
-		int a = 10;
-		int b = 20;
-		System.out.println("Beforw Swap ; a"+a+",b="+b);
-		int temp = a;
-		      a = b;
-		      b = temp;
-		      System.out.println("After swap : a"+a+",b="+b);
+		int alpha = 10;
+		int beta = 20;
+		System.out.println("Beforw Swap ; alpha"+alpha+",beta="+beta);
+		int temprory = alpha;
+		      alpha = beta;
+		      beta = temprory;
+		      System.out.println("After swap : alpha"+alpha+",beta="+beta);
 
 	}
 
