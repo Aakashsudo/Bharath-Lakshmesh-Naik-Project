12696882b44895916b02a424a87dc206f69bf6ea
Aakashsudo <79778923+Aakashsudo@users.noreply.github.com>
2025-12-04 13:48:48 +0530

    Update TrainglePattern.java

--- TrainglePattern.java
+++ TrainglePattern.java
@@ -7,12 +7,12 @@ public class TrainglePattern {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the number of rows:");
-		int n =sc.nextInt();
-		for(int i=1;i<=n;i++)
+		int number =sc.nextInt();
+		for(int integer =1;integer<=n;integer++)
 		{
-			for(int j=1;j<=i;j++)
+			for(int java=1;java<=integer;java++)
 			{
-				System.out.print(j);
+				System.out.print(java);
 			}
 			System.out.println();
 			}
