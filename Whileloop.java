9cb6ac4bdef4081da131e421370140ee8f1b1cb8
Aakashsudo <79778923+Aakashsudo@users.noreply.github.com>
2025-12-04 13:50:56 +0530

    Update Whileloop.java

--- Whileloop.java
+++ Whileloop.java
@@ -3,11 +3,11 @@ package year;
 public class whileloop1 {
 
 	public static void main(String[] args) {
-	int x=10;
-	while(x<=20)
+	int xander=10;
+	while(xander<=20)
 	{
-		System.out.println("x ="+x);
-		x++;
+		System.out.println("xander ="+xander);
+		xander++;
 	}
 
 	}
