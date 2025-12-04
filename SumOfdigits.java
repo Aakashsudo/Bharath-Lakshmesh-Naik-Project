a00e7cb7b856ac26bbb01383d7b44b7fa9a70882
Aakashsudo <79778923+Aakashsudo@users.noreply.github.com>
2025-12-04 13:44:26 +0530

    Update SumOfdigits.java

--- SumOfdigits.java
+++ SumOfdigits.java
@@ -5,14 +5,14 @@ public class SumofDigits {
 	public static void main(String[] args) {
 		        Scanner sc = new Scanner(System.in);
 		        System.out.print("Enter a number: ");
-		        int n = sc.nextInt();
+		        int number = sc.nextInt();
 
 		        int sum = 0;
 
-		        while (n > 0) {
-		            int digit = n % 10;   
+		        while (number > 0) {
+		            int digit = number % 10;   
 		            sum = sum + digit;    
-		            n = n / 10;           
+		            number = number / 10;           
 		        }
 
 		        System.out.println("Sum of digits = " + sum);
