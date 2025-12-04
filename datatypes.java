499d3b7ef50d0d0fe3e78a5af9a3799f2c5ee717
Aakashsudo <79778923+Aakashsudo@users.noreply.github.com>
2025-12-04 14:00:12 +0530

    Update datatypes.java

--- datatypes.java
+++ datatypes.java
@@ -3,16 +3,16 @@ package year;
 public class Example {
 
 	public static void main(String[] args) {
-		int x = 10000;
-		int y = 20000;
-		long l = x*y;
-		float f = 15/10;
-		double d = x*y;
-		String s ="AKASH Tehcattyyy";
-		System.out.println("x*y :" +l);
-		System.out.println("x*y :" +d);
-		System.out.println("x/y :" +f);
-		System.out.println("s :" +s);
+		int xander = 10000;
+		int yawn = 20000;
+		long longs = xander*yawn;
+		float floats = 15/10;
+		double data = xander*yawn;
+		String strings ="AKASH Tehcattyyy";
+		System.out.println("xander*yawn :" +longs);
+		System.out.println("xander*yawn :" +data);
+		System.out.println("xander/yawn :" +floats);
+		System.out.println("strings :" +strings);
 		
 	}
 
