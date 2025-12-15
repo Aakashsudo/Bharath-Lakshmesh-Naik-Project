214ad30cee960fb3842aa04f2196f6f949aaf017
BHARTH LAKSHMESH NAIK <79778923+Aakashsudo@users.noreply.github.com>
2025-12-15 21:46:06 +0530

    Create Ifprogram.java

---
+++ Ifprogram.java
@@ -0,0 +1,19 @@
+package year;
+import java.util.Scanner;
+public class Ifprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num < 0) {
+            System.out.println("Absolute value: " + (-num));
+        } else {
+            System.out.println("Absolute value: " + num);
+	}
+
+}
+}
